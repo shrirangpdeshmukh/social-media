2f0781a3a66f6fd40c16c727b73fd276bf4d48fe
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2022-03-27 10:18:37 +0530

    Initialize project using Create React App

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGtoGY-9H_dqgLW8s8HdIoj3d3gwWqsBA",
+  authDomain: "social-media-1695a.firebaseapp.com",
+  projectId: "social-media-1695a",
+  storageBucket: "social-media-1695a.appspot.com",
+  messagingSenderId: "909138867369",
+  appId: "1:909138867369:web:c9a32efc1aca80f48125b8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
