ee72d78f64ae246dedccacad731e7312dfd114d9
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2022-03-28 16:16:57 +0530

    Post route changes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const postController = require("../controllers/postController");
 const fileController = require("../controllers/fileController");
+const authController = require("../controllers/authController");
 const Router = express.Router();
 
 Router.get("/", postController.getAllPosts);
+
+Router.use(authController.verifyJwtToken, authController.loggedInUser);
+
 Router.post("/", fileController.uploadFiles, postController.createPost);
-Router.delete("/", postController.deletePosts);
 
 Router.get("/:postId", postController.getSinglePost);
 Router.patch("/:postId", postController.updatePost);
 Router.delete("/:postId", postController.deletePost);
 
+Router.delete("/", postController.deletePosts);
 module.exports = Router;
