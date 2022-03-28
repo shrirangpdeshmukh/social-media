6ec9ed3ad965060510ff1e7dfeee620166953478
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2022-03-28 14:27:18 +0530

    Added User Controller & Post, user, Comment routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const authController = require("../controllers/authController");
+const userController = require("../controllers/userController");
+
+const Router = express.Router();
+
+Router.use(authController.verifyJwtToken, authController.loggedInUser);
+
+Router.get(userController.getUser);
+Router.patch("/bio", userController.addBio);
+
+module.exports = Router;
