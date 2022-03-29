f5e0f38543eb43df7d0449d5690e648ba52b912e
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2022-03-29 21:18:00 +0530

    Corrected Import

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,6 @@ import Layout from "./Layout";
 import Login from "./Login";
 import Post from "./Post";
 import Profile from "./Profile";
+import Hashtag from "./Hashtag";
 
-export { Create, Home, Layout, Login, Post, Profile };
+export { Create, Home, Layout, Login, Post, Profile, Hashtag };
